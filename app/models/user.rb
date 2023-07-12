0680dd8744e0ee69aec9d91f05c6f8f9d671bc75
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-07-12 11:39:16 +0300

    Add controller spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  attribute :name, :string
+  attribute :photo, :string
+  attribute :bio, :string
+
   has_many :posts, foreign_key: :author_id
   has_many :likes, foreign_key: :author_id
   has_many :comments, foreign_key: :author_id

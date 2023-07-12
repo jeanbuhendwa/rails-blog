cd0dee8a87b386dee181b67bf01d2061efb7fd89
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-07-12 11:40:05 +0300

    Fix linters

--- spec/requests/post_request_spec.rb
+++ spec/requests/post_request_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   let!(:user) { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
-  let!(:valid_attributes) {
+  let!(:valid_attributes) do
     {
       title: 'Hello',
       text: 'This is my first post',
       author_id: user.id
     }
-  }
+  end
 
   describe 'GET /index' do
     it 'returns a successful response' do
